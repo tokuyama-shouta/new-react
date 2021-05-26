921a788a0993f2ee5eb532252ef230df2eb1f5bf
tokuyama shouta <toku1031ts@gmail.com>
2021-05-26 21:09:56 +0900

    const let アロー関数

--- src/App.js
+++ src/App.js
@@ -2,6 +2,46 @@
 import './App.css';
 import Articl from "./components/Articl";
 
+// const testScope = () => {
+//   if(scope === 'function') {
+//     var functionScope = "関数スコープ内ならどこからでも参照できます"
+//   }else if (scope === 'block') {
+//     let blockScope = "ブロックスコープ内でしか参照できません"
+//   }
+//   console.log(functionScope);
+// }
+// testScope('function');
+
+let mutableText = 'let変更前';
+mutableText = 'let変更後';
+console.log(mutableText);
+
+const immutableText = 'const変更前';
+console.log(immutableText);
+
+const mutableArray = [1,2,3];
+mutableArray.push(4)
+console.log(mutableArray);
+
+const mutableObject = {id: 'hoge', value: 'fuga'}
+mutableObject['name'] = 'bar';
+console.log(mutableObject);
+
+function nameFunc(message) {
+  console.log(message);
+  
+}
+const arrowFunc = (message) => {
+  console.log(message);
+}
+const oneLineArrowFunc = (message) => console.log(message);
+
+nameFunc('これは名前付き関数です');
+arrowFunc('これはアロー関数です');
+oneLineArrowFunc('これは一行で書いたアロー関数です');
+
+
+
 function App() {
   return (
     <div className="App">
