67534cd379b883eb965b29668799a1728531f0ef
tokuyama shouta <toku1031ts@gmail.com>
2021-06-01 20:35:36 +0900

    useState

--- src/components/Articl.jsx
+++ src/components/Articl.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {useState} from "react";
+
+
 
 const Articl = (props) => {
+  const [isPublished, setIsPublished] = useState(false)
+  console.log(isPublished)
   return (
     <div>
       <h2>{props.title}</h2>
       <h2>{props.content}</h2>
+      <button onClick={() => setIsPublished(true)}>公開</button>
     </div>
   );
 };
